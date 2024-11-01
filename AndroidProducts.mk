c4908bca3d08eb034532ebb9dbf78087af02d9d5
ELbee <85691313+woodyfi@users.noreply.github.com>
2024-11-01 19:49:58 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lavender.mk
+    $(LOCAL_DIR)/afterlife_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lavender-user \
-    lineage_lavender-userdebug \
-    lineage_lavender-eng
+    afterlife_lavender-user \
+    afterlife_lavender-userdebug \
+    afterlife_lavender-eng
